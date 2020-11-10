5c9e3e094fdac1834666154503d8be2b4afdb81f
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-11-10 11:30:29 +0000

    import bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,13 @@ import Home from './components/common/Home'
 import Navbar from './components/common/Navbar'
 import Register from './components/common/Register'
 import Login from './components/common/Login'
-import QuestShow from './components/quests/QuestShow'
-import QuestCreate from './components/quests/QuestCreate'
 import ProfileShow from './components/user/ProfileShow'
 import ProfileIndex from './components/user/ProfileIndex'
 import UserProfile from './components/user/UserProfile'
 
-
-import QuestIndex from './components/quests/QuestIndex'
+import QuestCreate from './components/quest/QuestCreate'
+import QuestShow from './components/quest/QuestShow'
+import QuestIndex from './components/quest/QuestIndex'
 
 
 const App = () => {
